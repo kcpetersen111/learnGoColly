f51e67a34970160211f9585f7bf520f84fc2fec5
Kaleb Petersen <kcpetersen@LAPTOP-6C7T2E9V.localdomain>
2022-05-12 13:59:30 -0600

    adding the citScrapper

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ To change what file is ran just go into main.go and change what function is call
 ## Currently added functions:
 	exampleCourseraCourses: An example from the colly website that will get all of the courses from the coursera website
 
-	movieReview: My first scrapper. Will take input from the commandline and return what the imdb top movies with the given titles and the ratings for those movies.
+	movieReview: My first scrapper. Will take input from the commandline and return what the imdb top movies with the given titles and the ratings for those movies
+	
+	citScrapper: Will check the dsu cit website for dead links
---
+++ citScraper.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"github.com/gocolly/colly"
+	"fmt"
+	"log"
+	"strconv"
+)
+
+//will traverse all of the dead links on the dsu cs cit page and print them to the screen
+
+func citScrapper() {
+	depth, err:= getInput("How deep would you like to search?")
+	if err != nil{
+		// fmt.Println(err)
+		log.Fatal(err)
+	}
+	intDepth,err:= strconv.Atoi(depth)
+	if err != nil{
+		log.Fatal(err)
+	}
+	// finMap := make(map[string]bool)
+	// dead := make([]string,1)
+	// parentMap := make(map[string]string)
+	dead := make( map[string]error )
+	c := colly.NewCollector(
+		colly.AllowedDomains("cit.dixie.edu", "https://cit.dixie.edu/","www.cit.dixie.edu"),
+		colly.MaxDepth(intDepth),
+	)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		
+		link := e.Attr("href")
+		// if !strings.HasPrefix(link, "/title") {
+		// 	return
+		// }	
+		// parentMap[link] = e.Request.URL.String()
+		e.Request.Visit(link)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		// finMap[r.URL.String()] = true
+		log.Println("visiting", r.URL.String())
+	})
+
+	c.OnResponse(func(r *colly.Response){
+		// finMap[r.Request.URL.String()] = false
+	})
+
+	c.OnError(func(r *colly.Response,err error){
+		// fmt.Println("something went wrong with ",r.Request.URL.String())
+		// dead = append(dead, r.Request.URL.String())
+		fmt.Println(err)
+		dead[r.Request.URL.String()] = err
+	})
+
+	c.Visit("https://cit.dixie.edu/cs/")
+
+	fmt.Printf("\nDONE\n\n")
+
+	for key, val := range dead{
+		fmt.Printf("The link %v is dead. The error was %v.\n", key,val)
+	}
+
+	fmt.Println(c)
+}
